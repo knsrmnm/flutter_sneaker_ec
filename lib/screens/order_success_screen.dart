2cc0ea69f3ed5c0d13917f98fbddb7e1773bb111
Ken Minami <kenminami.dev@gmail.com>
2022-05-22 10:07:49 +0900

    add logo image.

---
+++ lib/screens/order_success_screen.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class OrderSuccessScreen extends StatefulWidget {
+  const OrderSuccessScreen({Key? key}) : super(key: key);
+
+  @override
+  State<OrderSuccessScreen> createState() => _OrderSuccessScreenState();
+}
+
+class _OrderSuccessScreenState extends State<OrderSuccessScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Completed", style: TextStyle(fontSize: 15.0, fontWeight: FontWeight.w700, color: Colors.black)),
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.all(30.0),
+          child: Center(
+            child: Column(
+              children: [
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
